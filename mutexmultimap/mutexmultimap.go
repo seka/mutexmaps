ceb9ba484386fa0466c825b8fe36917c575fd8f8
seka <s.seka134704@gmail.com>
2016-02-11 02:04:42 +0900

    mutexmultimap: Add mutexmultimap

---
+++ mutexmultimap/mutexmultimap.go
@@ -0,0 +1,199 @@
+package maputil
+
+import (
+	"sync"
+)
+
+// MutexMultiMap that maps keys to values, similar to Map
+// but in which each key may be associated with multiple values.
+// a → 1, 2
+// b → 3
+type MutexMultiMap struct {
+	sync.RWMutex
+	hint  int
+	items map[string][]interface{}
+}
+
+// New is sole constructor.
+func New(hint int) *MutexMultiMap {
+	return &MutexMultiMap{
+		hint:  hint,
+		items: make(map[string][]interface{}, hint),
+	}
+}
+
+// Clear is removes all key-value pairs from the multimap, leaving it empty.
+func (m *MutexMultiMap) Clear() {
+	m.Lock()
+	m.items = make(map[string][]interface{}, m.hint)
+	m.Unlock()
+}
+
+// ContainsEntry is returns true if this multimap contains
+// at least one key-value pair with the key key and the value value.
+func (m *MutexMultiMap) ContainsEntry(key string, value interface{}) bool {
+	m.RLock()
+	defer m.RUnlock()
+	items, ok := m.items[key]
+	if !ok {
+		return false
+	}
+	for i := range items {
+		if items[i] == value {
+			return true
+		}
+	}
+	return false
+}
+
+// ContainsKey is returns true if this multimap contains at least one key-value
+// pair with the key key.
+func (m *MutexMultiMap) ContainsKey(key string) bool {
+	m.RLock()
+	_, isExist := m.items[key]
+	m.RUnlock()
+	return isExist
+}
+
+// ContainsValue is returns true if this multimap contains at least one
+// key-value pair with the value value.
+func (m *MutexMultiMap) ContainsValue(value interface{}) bool {
+	m.RLock()
+	defer m.RUnlock()
+	for _, items := range m.items {
+		for i := range items {
+			if items[i] == value {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// Entries is returns a view collection of all key-value pairs contained in
+// this multimap, as Map.Entry instances.
+func (m *MutexMultiMap) Entries() map[string][]interface{} {
+	m.RLock()
+	src := m.items
+	dst := make(map[string][]interface{}, len(src))
+	for k, v := range src {
+		dst[k] = v
+	}
+	m.RUnlock()
+	return dst
+}
+
+// Equals is compares the specified object with this multimap for equality.
+func (m *MutexMultiMap) Equals(src map[string][]interface{}) bool {
+	m.RLock()
+	defer m.RUnlock()
+	if len(m.items) != len(src) {
+		return false
+	}
+	for k, items := range m.items {
+		for i := range items {
+			if m.items[k][i] != src[k][i] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// Get is returns a view collection of the values associated with key in
+// this multimap, if any.
+func (m *MutexMultiMap) Get(key string) []interface{} {
+	m.RLock()
+	src := m.items[key]
+	dst := make([]interface{}, len(src))
+	for i := range src {
+		dst[i] = src[i]
+	}
+	m.RUnlock()
+	return dst
+}
+
+// IsEmpty is returns true if this multimap contains no key-value pairs.
+func (m *MutexMultiMap) IsEmpty() bool {
+	return m.Len() == 0
+}
+
+// Put is stores a key-value pair in this multimap.
+func (m *MutexMultiMap) Put(key string, value interface{}) {
+	m.Lock()
+	items := m.items[key]
+	items = append(items, value)
+	m.items[key] = items
+	m.Unlock()
+}
+
+// PutAll is stores a key-value pair in this multimap for each of values
+// all using the same key, key.
+func (m *MutexMultiMap) PutAll(key string, values []interface{}) {
+	m.Lock()
+	m.items[key] = values
+	m.Unlock()
+}
+
+// Delete is removes a single key-value pair with the key key and
+// the value value from this multimap, if such exists.
+func (m *MutexMultiMap) Delete(key string, value interface{}) {
+	m.Lock()
+	defer m.Unlock()
+	items, ok := m.items[key]
+	if !ok {
+		return
+	}
+	cutSlice(&items, value)
+	if len(items) == 0 {
+		delete(m.items, key)
+		return
+	}
+	m.items[key] = items
+}
+
+// DeleteAll is removes all values associated with the key key.
+func (m *MutexMultiMap) DeleteAll(key string) []interface{} {
+	m.Lock()
+	defer m.Unlock()
+	items, ok := m.items[key]
+	if !ok {
+		return nil
+	}
+	m.items[key] = make([]interface{}, 0, len(items))
+	return items
+}
+
+// ReplaceValues is stores a collection of values with the same key,
+// replacing any existing values for that key.
+func (m *MutexMultiMap) ReplaceValues(key string, srcs ...interface{}) []interface{} {
+	m.Lock()
+	defer m.Unlock()
+	_, ok := m.items[key]
+	if !ok {
+		return nil
+	}
+	m.items[key] = srcs
+	return m.items[key]
+}
+
+// Len is returns the number of key-value pairs in this multimap.
+func (m *MutexMultiMap) Len() int {
+	m.RLock()
+	l := len(m.items)
+	m.RUnlock()
+	return l
+}
+
+func cutSlice(ps *[]interface{}, value interface{}) {
+	items := *ps
+	for i := 0; i < len(items); {
+		if items[i] == value {
+			copy(items[i:], items[i+1:])
+			items = items[:len(items)-1]
+			continue
+		}
+		i++
+	}
+	*ps = items
+}
---
+++ mutexmultimap/mutexmultimap_test.go
@@ -0,0 +1,171 @@
+package maputil
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMutexMultiMapNew(t *testing.T) {
+	m := New(1)
+	if !m.IsEmpty() {
+		t.Fatal("length should be 0:", m.Len())
+	}
+}
+
+func TestMutexMultiMapClear(t *testing.T) {
+	m := New(1)
+	m.Put("a", "value")
+	if l := m.Len(); l != 1 {
+		t.Fatal("invalid length:", l)
+	}
+	m.Clear()
+	if l := m.Len(); l != 0 {
+		t.Fatal("invalid length:", l)
+	}
+}
+
+func TestMutexMultiMapContainsEntry(t *testing.T) {
+	m := New(1)
+	if ok := m.ContainsEntry("a", "value"); ok {
+		t.Fatal("ContainsEntry should be false:", ok)
+	}
+	m.Put("a", "value")
+	if ok := m.ContainsEntry("a", "value"); !ok {
+		t.Fatal("ContainsEntry should be true:", ok)
+	}
+}
+
+func TestMutexMultiMapContainsKey(t *testing.T) {
+	m := New(1)
+	if ok := m.ContainsKey("a"); ok {
+		t.Fatal("ContainsEntry should be false:", ok)
+	}
+	m.Put("a", "value")
+	if ok := m.ContainsKey("a"); !ok {
+		t.Fatal("ContainsEntry should be true:", ok)
+	}
+}
+
+func TestMutexMultiMapContainsValue(t *testing.T) {
+	m := New(1)
+	if ok := m.ContainsValue("value"); ok {
+		t.Fatal("ContainsValue should be false:", ok)
+	}
+	m.Put("a", "value")
+	if ok := m.ContainsValue("value"); !ok {
+		t.Fatal("ContainsEntry should be true:", ok)
+	}
+}
+
+func TestMutexMultiMapEntries(t *testing.T) {
+	m := New(1)
+	m.Put("a", "value")
+	if l := m.Len(); l != 1 {
+		t.Fatal("mutexmap length should be 1:", l)
+	}
+	values := m.Entries()
+	expected := map[string][]interface{}{
+		"a": {"value"},
+	}
+	if !reflect.DeepEqual(values, expected) {
+		t.Fatal("invalid values:", values, expected)
+	}
+}
+
+func TestMutexMultiMapEquals(t *testing.T) {
+	m := New(1)
+	m.Put("a", "value")
+	items1 := map[string][]interface{}{}
+	if m.Equals(items1) {
+		t.Fatal("Equals should be false", m.Entries())
+	}
+	items2 := map[string][]interface{}{
+		"a": {"value"},
+	}
+	if !m.Equals(items2) {
+		t.Fatal("invalid values:", m.Entries())
+	}
+}
+
+func TestMutexMultiMapPut(t *testing.T) {
+	m := New(1)
+	m.Put("a", "value1")
+	m.Put("a", "value2")
+	values := m.Get("a")
+	expected := []interface{}{"value1", "value2"}
+	if !reflect.DeepEqual(values, expected) {
+		t.Fatal("invalid values:", values, expected)
+	}
+}
+
+func TestMutexMultiMapPutAll(t *testing.T) {
+	m := New(1)
+	values := []interface{}{"value1", "value2"}
+	m.PutAll("a", values)
+	items := m.Get("a")
+	if !reflect.DeepEqual(items, values) {
+		t.Fatal("invalid values:", items, values)
+	}
+}
+
+func TestMutexMultiMapDelete(t *testing.T) {
+	m := New(1)
+	m.Put("a", "value1")
+	m.Put("a", "value2")
+	if l := m.Len(); l != 1 {
+		t.Fatal("mutexmap length should be 1:", l)
+	}
+	m.Delete("a", "value1")
+	actual := m.Get("a")
+	expected := []interface{}{"value2"}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatal("invalid values:", actual, expected)
+	}
+}
+
+func TestMutexMultiMapDeleteAllValues(t *testing.T) {
+	m := New(1)
+	m.Put("a", "value")
+	if l := m.Len(); l != 1 {
+		t.Fatal("mutexmap length should be 1:", l)
+	}
+	m.Delete("a", "value")
+	if l := m.Len(); l != 0 {
+		t.Fatal("mutexMultiMap length should be 0:", l)
+	}
+	values := m.Get("a")
+	expected := []interface{}{}
+	if !reflect.DeepEqual(values, expected) {
+		t.Fatal("invalid values1:", values, expected)
+	}
+}
+
+func TestMutexMultiMapDeleteAll(t *testing.T) {
+	m := New(1)
+	m.Put("a", "value1")
+	if l := m.Len(); l != 1 {
+		t.Fatal("mutexmap length should be 1:", l)
+	}
+	m.DeleteAll("a")
+	actual := m.Get("a")
+	expected := []interface{}{}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatal("invalid values:", actual, expected)
+	}
+}
+
+func TestMutexMultiMapReplaceValues(t *testing.T) {
+	m := New(1)
+	m.Put("a", "value1")
+	actual1 := m.Get("a")
+	expected1 := []interface{}{"value1"}
+	if !reflect.DeepEqual(actual1, expected1) {
+		t.Fatal("invalid values:", actual1, expected1)
+	}
+	m.ReplaceValues("a", "value2")
+	actual2 := m.Get("a")
+	expected2 := []interface{}{"value2"}
+	if !reflect.DeepEqual(actual2, expected2) {
+		t.Fatal("invalid values:", actual2, expected2)
+	}
+}
